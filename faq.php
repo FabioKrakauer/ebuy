0e6bcdc7d5c56c07dab1c0725e0b434416b712ca
BERNARDO DIAS <bernardo412@gmail.com>
2018-07-27 17:35:37 -0300

    Atualiza classes css

--- faq.php
+++ faq.php
@@ -34,57 +34,57 @@
 	</header>
 	<div class="flex-container">
 		<h1 class="title">FAQ - Perguntas Frequentes</h1>
-		<div class="articles">
-			<div class="flex-line">
-				<div class="content">
-				<p class="faq-text-title">Existe uma quantidade máxima no meu pedido?</p>
+		<div class="flex-articles">
+			<div class="flex-articles-line">
+				<div class="faq">
+				<p class="faq-title">Existe uma quantidade máxima no meu pedido?</p>
 				<p class="faq-text">Não, a quantidade de items por pedido é ilimitada</p>
 				</div>
 				<hr>
-				<div class="content">
-				<p class="faq-text-title">Como faço para receber as novidades?</p>
+				<div class="faq">
+				<p class="faq-title">Como faço para receber as novidades?</p>
 				<p class="faq-text">Basta cadastrar em nosso site e pronto, você já receberá as novidades da nossa linha de produtos</p>
 				</div>
 			</div>
-			<div class="flex-line">
+			<div class="flex-articles-line">
 				<hr class="line-hr">
 			</div>
-			<div class="flex-line">
-				<div class="content">
-				<p class="faq-text-title">Posso fazer uma visita à fábrica?</p>
+			<div class="flex-articles-line">
+				<div class="faq">
+				<p class="faq-title">Posso fazer uma visita à fábrica?</p>
 				<p class="faq-text">Não. Visitas não são abertas ao público, por se tratar de uma indústria de alimentos que precisa seguir as normas e especificações dos órgãos regulamentadores, como por exemplo a Vigilância Sanitária.</p>
 				</div>
 				<hr>
-				<div class="content">
-				<p class="faq-text-title">Como obter informações nutricionais dos produtos?</p>
+				<div class="faq">
+				<p class="faq-title">Como obter informações nutricionais dos produtos?</p>
 				<p class="faq-text">Nós disponibilizamos a tabela nutricional de todos os seus produtos de linha aqui no site. Verifique na página do produto de seu interesse</p>
 				</div>
 			</div>
-			<div class="flex-line">
+			<div class="flex-articles-line">
 				<hr class="line-hr">
 			</div>
-			<div class="flex-line">
-				<div class="content">
-				<p class="faq-text-title">Quais são as formas de pagamentos disponibilizados?</p>
+			<div class="flex-articles-line">
+				<div class="faq">
+				<p class="faq-title">Quais são as formas de pagamentos disponibilizados?</p>
 				<p class="faq-text">Cartão de crédito das bandeiras VISA, MASTERCARD, DINNERS e AMERICAN EXPRESS. Débito automático e boleto bancário.</p>
 				</div>
 				<hr>
-				<div class="content">
-				<p class="faq-text-title">Como é calculado a taxa do frete</p>
+				<div class="faq">
+				<p class="faq-title">Como é calculado a taxa do frete</p>
 				<p class="faq-text">O próprio site já calcula o frete para você, basta informar o endereço de entrega</p>
 				</div>
 			</div>
-			<div class="flex-line">
+			<div class="flex-articles-line">
 				<hr class="line-hr">
 			</div>
-			<div class="flex-line">
-				<div class="content">
-				<p class="faq-text-title">Quanto tempo demora o pedido?</p>
+			<div class="flex-articles-line">
+				<div class="faq">
+				<p class="faq-title">Quanto tempo demora o pedido?</p>
 				<p class="faq-text">Demora até 5 dias úteis</p>
 				</div>
 				<hr>
-				<div class="content">
-				<p class="faq-text-title">Posso alterar o pedido após ele ter sido finalizado</p>
+				<div class="faq">
+				<p class="faq-title">Posso alterar o pedido após ele ter sido finalizado</p>
 				<p class="faq-text">Não trabalhamos com esta opção, é recomendado revisar o seu pedido antes de finalizá-lo</p>
 				</div>
 			</div>
