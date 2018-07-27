72a05e9abf58383865c0aea50f2046181234687d
BERNARDO DIAS <bernardo412@gmail.com>
2018-07-27 16:38:48 -0300

    Atualiza classes css e deleta arquivo inutil

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/index.css">
-    <!-- <link rel="stylesheet" href="css/index.css"> -->
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"> -->
     <title>Home</title>
   </head>
   <body>
     <header class="flex-header">
-        <label class="logo"><a class="logo-a" href="index.php">E-Buy</a></label>
-        <label for="toggle-menu"><img id="toggle-menu-icon" src="images/toggle.png" alt=""></label>
+        <label class="header-logo">
+          <a class="logo-a" href="index.php">E-Buy</a>
+        </label>
+        <label for="toggle-menu"><img class="toggle-menu-icon" src="images/toggle.png" alt=""></label>
         <input type="checkbox" id="toggle-menu">
         <div class="navigation">
           <ul class="nav-ul">
@@ -32,37 +32,35 @@
         </div>
     </header>
     <div class="flex-container">
-      <div class="banner">
-        <img src="images/banner-1.jpg" alt="banner">
+      <div class="flex-banner">
+        <img class="banner-img" src="images/banner-1.jpg" alt="banner">
       </div>
-    </div>
-    <div class="articles">
-      <article class="product">
-        <img src="images/produto-2.jpg" alt="produto 02">
-        <h2>Produtos diversificados</h2>
-        <p>A nossa loja possui uma grande variedade de chocolates</p>
-      </article>
-      <article class="product">
-        <img src="images/produto-3.jpg" alt="produto 03">
-        <h2>Arte em forma de chocolate</h2>
-        <p>Nossos produtos são de uma beleza incomparável, Você vai começar a comer com os olhos </p>
-      </article>
-      <article class="product">
-        <img src="images/produto-4.jpg" alt="produto 04">
-        <h2>Entregamos em qualquer lugar</h2>
-        <p>Encomende nossos chocolates conosco</p>
-      </article>
-      <article class="product">
-        <img src="images/produto-5.jpg" alt="pdto 02">
-        <h2>Sabor e Qualidade</h2>
-        <p>Os produtos passam por um rigoroso controle de qualidade,
-        sempre inovando para trazer para você o melhor chocolate</p>
-      </article>
-    </div>
-    <footer>
-      <div class="footer">
-        Copyright 2018 - Todos os direitos reservados -
+      <div class="flex-articles">
+        <article class="articles-product">
+          <img class="product-img" src="images/produto-2.jpg" alt="produto 02">
+          <h2 class="product-h2">Produtos diversificados</h2>
+          <p class="product-p">A nossa loja possui uma grande variedade de chocolates</p>
+        </article>
+        <article class="articles-product">
+          <img class="product-img" src="images/produto-3.jpg" alt="produto 03">
+          <h2 class="product-h2">Arte em forma de chocolate</h2>
+          <p class="product-p">Nossos produtos são de uma beleza incomparável, Você vai começar a comer com os olhos </p>
+        </article>
+        <article class="articles-product">
+          <img class="product-img" src="images/produto-4.jpg" alt="produto 04">
+          <h2 class="product-h2">Entregamos em qualquer lugar</h2>
+          <p class="product-p">Encomende nossos chocolates conosco</p>
+        </article>
+        <article class="articles-product">
+          <img class="product-img" src="images/produto-5.jpg" alt="pdto 02">
+          <h2 class="product-h2">Sabor e Qualidade</h2>
+          <p class="product-p">Os produtos passam por um rigoroso controle de qualidade,
+          sempre inovando para trazer para você o melhor chocolate</p>
+        </article>
       </div>
-    </footer>
+      <footer class="footer">
+          Copyright 2018 - Todos os direitos reservados -
+      </footer>
+    </div>
   </body>
 </html>
