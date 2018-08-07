8996195f477cbd9417efd7115b1b83a19dab90eb
Andre Bensemana <andrebensemana@gmail.com>
2018-08-07 19:35:47 -0300

    upload-foto e modelo de dados

--- register.php
+++ register.php
@@ -34,6 +34,22 @@
     </header>
     <div class="flex-container">
       <form class="flex-form">
+
+        <!--André-->
+        <div class="caixa">
+            <div class="caixa-interna">
+              <div class="caixa-texto">
+                <h5>Sua foto de perfil</h5>
+              </div>
+              <img src="images/rosto.png" width="110" height="110" alt="avatar"
+              class="img-avatar">
+            </div>
+        </div>
+        <div class="botao">
+           <input type="file" name="botao" value="Alterar Foto" class="btn">
+        </div>
+        <!--André-->
+
         <div class="flex-form-item">
           <label class="item-label" for="email">E-mail: <span class="item-span">*</span></label>
           <input class="item-input" type="email" name="email" placeholder="Digite seu e-mail" >
