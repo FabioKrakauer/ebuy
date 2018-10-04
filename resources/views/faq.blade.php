9187c5f5941472c1fa34da7d1319ae3a715e962a
FabioKrakauer <comvocefabio@gmail.com>
2018-10-04 19:42:23 -0300

    Add faq blade and faq route

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Create Faq</title>
+</head>
+<body>
+a
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/faq', function(){
+	return view('faq');
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
