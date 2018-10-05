ca5f0cec81c580137f48866a249b686e110a9516
FabioKrakauer <comvocefabio@gmail.com>
2018-10-05 13:24:18 -0300

    Traduz arquivos do laravel

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Suas senhas tem que ter no minimo 6 caracteres e devem ser identicas',
+    'reset' => 'Sua senha foi redefinida com sucesso!',
+    'sent' => 'Enviamos um e-mail para você redefinir sua senha!',
+    'token' => 'Token de validação invalido!',
+    'user' => "Nenhum usuario com este e-mail!",
 
 ];
