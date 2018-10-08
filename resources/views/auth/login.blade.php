d70642f0ee9c09a34628432328618e393f96bb31
gregoryls1 <contatogregory@gmail.com>
2018-10-08 19:32:00 -0300

    add navbar PageLogin complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,25 +16,6 @@
           <a class="navbar-brand mr-auto" href="/">
             Logo
           </a>
-          <ul class="navbar-nav flex-row mr-lg-0">
-              <li class="nav-item">
-                  <a class="nav-link pr-2"><i class="fa fa-search"></i></a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link pr-2"><i class="fa fa-facebook"></i></a>
-              </li>
-              <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle mr-3 mr-lg-0" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i><span class="caret"></span>
-                  </a>
-                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="">Usuario</a>
-                      <a class="dropdown-item" href="">Login</a>
-                  </div>
-              </li>
-          </ul>
-          <button class="navbar-toggler ml-lg-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
       </nav>
     </header>
         <div class="container">
