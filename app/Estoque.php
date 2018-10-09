1778fdc0a7d5bbd7e2ed268c053c362791232b4c
unknown <andres@digitalhouse.com>
2018-10-08 21:17:44 -0300

    criação dos scripts de migração e atualização das models

--- app/Estoque.php
+++ app/Estoque.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Estoque extends Model
 {
     protected $table = 'produto';
-    protected $fillable = ['quantidade',''];
+    protected $fillable = ['produto_id','quantidade'];
 
   public function genero(){
     return $this->hasOne(Produto::class,'id','produto_id');
--- app/Produto.php
+++ app/Produto.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     protected $table = 'produto';
-    protected $fillable = ['nome','preco'];
+    protected $fillable = ['nome','preco','descricao'];
 }
