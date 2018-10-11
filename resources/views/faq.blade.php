4c646b694e23880b6628df7d95f9e22a342cdd0b
renato.yukio <renato.yukio@opus-software.com.br>
2018-10-11 16:19:57 -0300

    Criação da página faq na blade

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,9 +1,87 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html>
 <head>
 	<title>Create Faq</title>
 </head>
 <body>
 a
+</body>
+</html> --}}
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/faq.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+	<title>FAQ - E-buy</title>
+</head>
+<body>
+	<header>
+
+	</header>
+	<div class="flex-container">
+		<h1 class="title">FAQ - Perguntas Frequentes</h1>
+		<div class="flex-articles">
+			<div class="flex-articles-line">
+				<div class="faq">
+				<p class="faq-title">Existe uma quantidade máxima no meu pedido?</p>
+				<p class="faq-text">Não, a quantidade de items por pedido é ilimitada.</p>
+				</div>
+				<hr>
+				<div class="faq">
+				<p class="faq-title">Como faço para receber as novidades?</p>
+				<p class="faq-text">Basta cadastrar em nosso site e pronto, você já receberá as novidades da nossa linha de produtos</p>
+				</div>
+			</div>
+			<div class="flex-articles-line">
+				<hr class="line-hr">
+			</div>
+			<div class="flex-articles-line">
+				<div class="faq">
+				<p class="faq-title">Posso fazer uma visita à fábrica?</p>
+				<p class="faq-text">Não. Visitas não são abertas ao público, por se tratar de uma indústria de alimentos que precisa seguir as normas e especificações dos órgãos regulamentadores, como por exemplo a Vigilância Sanitária.</p>
+				</div>
+				<hr>
+				<div class="faq">
+				<p class="faq-title">Como obter informações nutricionais dos produtos?</p>
+				<p class="faq-text">Nós disponibilizamos a tabela nutricional de todos os seus produtos de linha aqui no site. Verifique na página do produto de seu interesse</p>
+				</div>
+			</div>
+			<div class="flex-articles-line">
+				<hr class="line-hr">
+			</div>
+			<div class="flex-articles-line">
+				<div class="faq">
+				<p class="faq-title">Quais são as formas de pagamentos disponibilizados?</p>
+				<p class="faq-text">Cartão de crédito das bandeiras VISA, MASTERCARD, DINNERS e AMERICAN EXPRESS. Débito automático e boleto bancário.</p>
+				</div>
+				<hr>
+				<div class="faq">
+				<p class="faq-title">Como é calculado a taxa do frete</p>
+				<p class="faq-text">O próprio site já calcula o frete para você, basta informar o endereço de entrega</p>
+				</div>
+			</div>
+			<div class="flex-articles-line">
+				<hr class="line-hr">
+			</div>
+			<div class="flex-articles-line">
+				<div class="faq">
+				<p class="faq-title">Quanto tempo demora o pedido?</p>
+				<p class="faq-text">Demora até 5 dias úteis</p>
+				</div>
+				<hr>
+				<div class="faq">
+				<p class="faq-title">Posso alterar o pedido após ele ter sido finalizado</p>
+				<p class="faq-text">Não trabalhamos com esta opção, é recomendado revisar o seu pedido antes de finalizá-lo</p>
+				</div>
+			</div>
+		</div>
+		<footer class="footer">
+			Copyright 2018 - Todos os direitos reservados -
+		</footer>
+	</div>
 </body>
 </html>
