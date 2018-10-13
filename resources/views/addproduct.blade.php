0aea8a03e71708a097da6b25b1d01a8346cb52eb
BERNARDO DIAS <bernardo412@gmail.com>
2018-10-13 12:47:51 -0300

    Erro no arquivo css

--- resources/views/addproduct.blade.php
+++ resources/views/addproduct.blade.php
@@ -33,8 +33,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/addproduct.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/register.css">
     <title>Cadastro</title>
   </head>
   <body>
