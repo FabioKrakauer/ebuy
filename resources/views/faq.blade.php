2ddd41bf9cfe635c8e4cf15d4a5cc5c191dc888c
renatomnm <renatomnm@gmail.com>
2018-10-16 18:04:36 -0300

    implementando o card no faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -31,127 +31,95 @@
 </header>
 <h1 class="container md-8">FAQ - Perguntas Frequentes</h1>
 	<div class="container md-3">
-		<div class="card" style="margin:5px;">
-			<div class="card-header">
+		<div class="card" style="margin:10px;">
+			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
 					Existe uma quantidade máxima no meu pedido?
 			</div>
-			<div class="card-body">
-				Não, a quantidade de items por pedido é ilimitada
-			</div>
-		</div>
-		<div class="card" style="margin:5px;">
-			<div class="card-header">
-					Existe uma quantidade máxima no meu pedido?
-			</div>
-			<div class="card-body">
-				Não, a quantidade de items por pedido é ilimitada
-			</div>
-		</div>
-		<div class="card" style="margin:5px;">
-			<div class="card-header">
-					Existe uma quantidade máxima no meu pedido?
-			</div>
-			<div class="card-body">
-				Não, a quantidade de items por pedido é ilimitada
-			</div>
-		</div>
-		<div class="card" style="margin:5px;">
-			<div class="card-header">
-					Existe uma quantidade máxima no meu pedido?
-			</div>
-			<div class="card-body">
-				Não, a quantidade de items por pedido é ilimitada
-			</div>
-		</div>
-		<div class="card" style="margin:5px;">
-			<div class="card-header">
-					Existe uma quantidade máxima no meu pedido?
-			</div>
-			<div class="card-body">
-				Não, a quantidade de items por pedido é ilimitada
-			</div>
-		</div>
-		<div class="card" style="margin:5px;">
-			<div class="card-header">
-					Existe uma quantidade máxima no meu pedido?
-			</div>
-			<div class="card-body">
-				Não, a quantidade de items por pedido é ilimitada
-			</div>
-		</div>
-		<div class="card" style="margin:5px;">
-			<div class="card-header">
-					Existe uma quantidade máxima no meu pedido?
-			</div>
-			<div class="card-body">
-				Não, a quantidade de items por pedido é ilimitada
-			</div>
-		</div>
-		<div class="card" style="margin:5px;">
-			<div class="card-header">
-					Existe uma quantidade máxima no meu pedido?
-			</div>
-			<div class="card-body">
+			<div class="card-body" style="padding:15px;">
 				Não, a quantidade de items por pedido é ilimitada
 			</div>
 		</div>
 	</div>
-		<!-- <div class="flex-articles">
-			<div class="flex-articles-line">
-				<div class="faq">
-				<p class="faq-title">Existe uma quantidade máxima no meu pedido?</p>
-				<p class="faq-text">Não, a quantidade de items por pedido é ilimitada.</p>
-				</div>
-				<hr>
-				<div class="faq">
-				<p class="faq-title">Como faço para receber as novidades?</p>
-				<p class="faq-text">Basta cadastrar em nosso site e pronto, você já receberá as novidades da nossa linha de produtos</p>
-				</div>
+	<div class="container md-3">
+		<div class="card" style="margin:10px;">
+			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+				Como faço para receber as novidades?
 			</div>
-			<div class="flex-articles-line">
-				<hr class="line-hr">
-			</div>
-			<div class="flex-articles-line">
-				<div class="faq">
-				<p class="faq-title">Posso fazer uma visita à fábrica?</p>
-				<p class="faq-text">Não. Visitas não são abertas ao público, por se tratar de uma indústria de alimentos que precisa seguir as normas e especificações dos órgãos regulamentadores, como por exemplo a Vigilância Sanitária.</p>
-				</div>
-				<hr>
-				<div class="faq">
-				<p class="faq-title">Como obter informações nutricionais dos produtos?</p>
-				<p class="faq-text">Nós disponibilizamos a tabela nutricional de todos os seus produtos de linha aqui no site. Verifique na página do produto de seu interesse</p>
-				</div>
-			</div>
-			<div class="flex-articles-line">
-				<hr class="line-hr">
-			</div>
-			<div class="flex-articles-line">
-				<div class="faq">
-				<p class="faq-title">Quais são as formas de pagamentos disponibilizados?</p>
-				<p class="faq-text">Cartão de crédito das bandeiras VISA, MASTERCARD, DINNERS e AMERICAN EXPRESS. Débito automático e boleto bancário.</p>
-				</div>
-				<hr>
-				<div class="faq">
-				<p class="faq-title">Como é calculado a taxa do frete</p>
-				<p class="faq-text">O próprio site já calcula o frete para você, basta informar o endereço de entrega</p>
-				</div>
-			</div>
-			<div class="flex-articles-line">
-				<hr class="line-hr">
-			</div>
-			<div class="flex-articles-line">
-				<div class="faq">
-				<p class="faq-title">Quanto tempo demora o pedido?</p>
-				<p class="faq-text">Demora até 5 dias úteis</p>
-				</div>
-				<hr>
-				<div class="faq">
-				<p class="faq-title">Posso alterar o pedido após ele ter sido finalizado</p>
-				<p class="faq-text">Não trabalhamos com esta opção, é recomendado revisar o seu pedido antes de finalizá-lo</p>
-				</div>
+			<div class="card-body" style="padding:15px;">
+				Basta cadastrar em nosso site e pronto, você já receberá as novidades da nossa linha de produtos
 			</div>
 		</div>
-	</div> -->
+	</div>
+	<div class="container md-3">
+		<div class="card" style="margin:10px;">
+			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+					Posso fazer uma visita à fábrica?
+			</div>
+			<div class="card-body" style="padding:15px;">
+					Não. Visitas não são abertas ao público, por se tratar de uma indústria de alimentos que precisa seguir as normas e especificações dos órgãos regulamentadores, como por exemplo a Vigilância Sanitária.
+			</div>
+		</div>
+	</div>
+	<div class="container md-3">
+		<div class="card" style="margin:10px;">
+			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+				Como obter informações nutricionais dos produtos?
+			</div>
+			<div class="card-body" style="padding:15px;">
+				Nós disponibilizamos a tabela nutricional de todos os seus produtos de linha aqui no site. Verifique na página do produto de seu interesse
+			</div>
+		</div>
+	</div>
+	<div class="container md-3">
+		<div class="card" style="margin:10px;">
+			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+					Quais são as formas de pagamentos disponibilizados?
+			</div>
+			<div class="card-body" style="padding:15px;">
+				Cartão de crédito das bandeiras VISA, MASTERCARD, DINNERS e AMERICAN EXPRESS. Débito automático e boleto bancário.
+			</div>
+		</div>
+	</div>
+	<div class="container md-3">
+		<div class="card" style="margin:10px;">
+			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+					Como é calculado a taxa do frete
+			</div>
+			<div class="card-body" style="padding:15px;">
+				O próprio site já calcula o frete para você, basta informar o endereço de entrega
+			</div>
+		</div>
+	</div>
+	<div class="container md-3">
+		<div class="card" style="margin:10px;">
+			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+					Quanto tempo demora o pedido?
+			</div>
+			<div class="card-body" style="padding:15px;">
+				Demora até 5 dias úteis
+			</div>
+		</div>
+	</div>
+	<div class="container md-3">
+		<div class="card" style="margin:10px;">
+			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+					Posso alterar o pedido após ele ter sido finalizado
+			</div>
+			<div class="card-body" style="padding:15px;">
+				Não trabalhamos com esta opção, é recomendado revisar o seu pedido antes de finalizá-lo
+			</div>
+		</div>
+	</div>
+	<div class="container md-3">
+		<div class="card" style="margin:10px;">
+			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+					Quanto tempo demora o pedido?
+			</div>
+			<div class="card-body" style="padding:15px;">
+				Não trabalhamos com esta opção, é recomendado revisar o seu pedido antes de finalizá-lo
+			</div>
+		</div>
+	</div>
 	<footer class="footer_home">
 			<div class="footer-copyright text-center py-3">© 2018 Copyright:
 			</div>
