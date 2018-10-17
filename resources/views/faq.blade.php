c8479d207057eecaf1837d5dcf407d7f6df0159d
renatomnm <renatomnm@gmail.com>
2018-10-16 21:14:58 -0300

    finalizando a tela de faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -30,99 +30,127 @@
 	</nav>
 </header>
 <h1 class="container md-8">FAQ - Perguntas Frequentes</h1>
-	<div class="container md-3">
-		<div class="card" style="margin:10px;">
-			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
-					Existe uma quantidade máxima no meu pedido?
-			</div>
-			<div class="card-body" style="padding:15px;">
-				Não, a quantidade de items por pedido é ilimitada
-			</div>
+
+<div id="accordion" class="container md-3">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Existe uma quantidade máxima no meu pedido?
+        </button>
+      </h5>
+    </div>
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        Não, a quantidade de items por pedido é ilimitada
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Como faço para receber as novidades?
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+      	Basta cadastrar em nosso site e pronto, você já receberá as novidades da nossa linha de produtos
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Posso fazer uma visita à fábrica?
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+				Não. Visitas não são abertas ao público, por se tratar de uma indústria de alimentos que precisa seguir as normas e especificações dos órgãos regulamentadores, como por exemplo a Vigilância Sanitária.
+      </div>
+    </div>
+  </div>
+	<div class="card">
+		<div class="card-header" id="headingFour">
+			<h5 class="mb-0">
+				<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+					Como obter informações nutricionais dos produtos?
+				</button>
+			</h5>
 		</div>
-	</div>
-	<div class="container md-3">
-		<div class="card" style="margin:10px;">
-			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
-				Como faço para receber as novidades?
-			</div>
-			<div class="card-body" style="padding:15px;">
-				Basta cadastrar em nosso site e pronto, você já receberá as novidades da nossa linha de produtos
-			</div>
-		</div>
-	</div>
-	<div class="container md-3">
-		<div class="card" style="margin:10px;">
-			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
-					Posso fazer uma visita à fábrica?
-			</div>
-			<div class="card-body" style="padding:15px;">
-					Não. Visitas não são abertas ao público, por se tratar de uma indústria de alimentos que precisa seguir as normas e especificações dos órgãos regulamentadores, como por exemplo a Vigilância Sanitária.
-			</div>
-		</div>
-	</div>
-	<div class="container md-3">
-		<div class="card" style="margin:10px;">
-			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
-				Como obter informações nutricionais dos produtos?
-			</div>
-			<div class="card-body" style="padding:15px;">
+		<div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+			<div class="card-body">
 				Nós disponibilizamos a tabela nutricional de todos os seus produtos de linha aqui no site. Verifique na página do produto de seu interesse
 			</div>
 		</div>
 	</div>
-	<div class="container md-3">
-		<div class="card" style="margin:10px;">
-			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+	<div class="card">
+		<div class="card-header" id="headingFive">
+			<h5 class="mb-0">
+				<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
 					Quais são as formas de pagamentos disponibilizados?
-			</div>
-			<div class="card-body" style="padding:15px;">
+				</button>
+			</h5>
+		</div>
+		<div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+			<div class="card-body">
 				Cartão de crédito das bandeiras VISA, MASTERCARD, DINNERS e AMERICAN EXPRESS. Débito automático e boleto bancário.
 			</div>
 		</div>
 	</div>
-	<div class="container md-3">
-		<div class="card" style="margin:10px;">
-			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+	<div class="card">
+		<div class="card-header" id="headingSix">
+			<h5 class="mb-0">
+				<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
 					Como é calculado a taxa do frete
-			</div>
-			<div class="card-body" style="padding:15px;">
+				</button>
+			</h5>
+		</div>
+		<div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+			<div class="card-body">
 				O próprio site já calcula o frete para você, basta informar o endereço de entrega
 			</div>
 		</div>
 	</div>
-	<div class="container md-3">
-		<div class="card" style="margin:10px;">
-			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+	<div class="card">
+		<div class="card-header" id="headingSeven">
+			<h5 class="mb-0">
+				<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
 					Quanto tempo demora o pedido?
-			</div>
-			<div class="card-body" style="padding:15px;">
-				Demora até 5 dias úteis
+				</button>
+			</h5>
+		</div>
+		<div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+			<div class="card-body">
+					Demora até 5 dias úteis
 			</div>
 		</div>
 	</div>
-	<div class="container md-3">
-		<div class="card" style="margin:10px;">
-			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
+	<div class="card">
+		<div class="card-header" id="heading8">
+			<h5 class="mb-0">
+				<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse8" aria-expanded="false" aria-controls="collapse8">
 					Posso alterar o pedido após ele ter sido finalizado
-			</div>
-			<div class="card-body" style="padding:15px;">
-				Não trabalhamos com esta opção, é recomendado revisar o seu pedido antes de finalizá-lo
+				</button>
+			</h5>
+		</div>
+		<div id="collapse8" class="collapse" aria-labelledby="heading8" data-parent="#accordion">
+			<div class="card-body">
+					Não trabalhamos com esta opção, é recomendado revisar o seu pedido antes de finalizá-lo
 			</div>
 		</div>
 	</div>
-	<div class="container md-3">
-		<div class="card" style="margin:10px;">
-			<div class="card-header" style="background:#6f008c; color:#FFFFFF; font-weight:bold">
-					Quanto tempo demora o pedido?
-			</div>
-			<div class="card-body" style="padding:15px;">
-				Não trabalhamos com esta opção, é recomendado revisar o seu pedido antes de finalizá-lo
-			</div>
-		</div>
-	</div>
+</div>
 	<footer class="footer_home">
 			<div class="footer-copyright text-center py-3">© 2018 Copyright:
 			</div>
 	</footer>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
